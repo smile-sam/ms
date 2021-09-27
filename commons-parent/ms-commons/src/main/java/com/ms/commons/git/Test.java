536abed5e561cfea4d71da8f313906401936ecd9
smile-sam <760448125@qq.com>
2021-09-27 21:56:36 +0800

    添加一个测试信息

---
+++ commons-parent/ms-commons/src/main/java/com/ms/commons/git/Test.java
@@ -0,0 +1,15 @@
+package com.ms.commons.git;/**
+ * @author sam on 2021/9/27 21:52.<br>
+ */
+
+/**
+ *
+ * @description
+ * @author sam
+ * @date 2021-09-27 21:52
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("this is a test!");
+    }
+}
